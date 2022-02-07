dc5d37733e0c141ec8980ababc8c2b30c331e6f9
KseniiaMarkiv <ksenya.markiv@gmail.com>
2022-02-07 23:22:08 +0200

    Created City obj

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_06_160502) do
+ActiveRecord::Schema.define(version: 2022_02_07_211759) do
+
+  create_table "cities", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "foos", force: :cascade do |t|
     t.string "name"
