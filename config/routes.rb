d3c2590147984c8b4fcaa2cc7af065f2d4e22072
KseniiaMarkiv <ksenya.markiv@gmail.com>
2022-02-09 16:14:18 +0200

    Added State routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :foos
   resources :cities
+  resources :states
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
