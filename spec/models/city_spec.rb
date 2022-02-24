0f125c0180b362bbffeaea310c9323c462655925
KseniiaMarkiv <ksenya.markiv@gmail.com>
2022-02-24 14:31:08 +0200

    Setup db cleaner

--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -1,9 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe City, type: :model do
+  let!(:before_count) { City.count }
   let(:city) { FactoryBot.create(:city) }
   
   context "City model" do
+    it "created Foo will be persisted and be found" do
+      expect(city).to be_persisted
+      expect(City.find(city.id)).to_not be_nil
+    end
     it "has a name" do
       city.validate 
       expect(city.errors[:city]).to_not include("can't be blank")    
