18af34d45f371e5bd652433f77f85e70720821e9
Kseniia_Markiv <ksenya.markiv@gmail.com>
2022-08-18 19:52:10 +0300

    API WhoAmI Service with User Roles

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
    
-  get 'authn/whoami'
+  get 'authn/whoami', defaults: { format: :json }
   get 'authn/checkme'
   mount_devise_token_auth_for 'User', at: 'auth', skip: %i[omniauth_callbacks: confirmable: lockable: timeoutable:]
 
-  namespace :api, defaults: {format: :json} do 
+  namespace :api, defaults: { format: :json } do 
     resources :foos, except: %i[new: edit:]
     resources :bars, except: %i[new: edit:]
     resources :images, except: %i[new: edit:] do 
-      post 'thing_images',  controller: :thing_images, action: :create
-      get 'thing_images',  controller: :thing_images, action: :image_things
-      get 'linkable_things',  controller: :thing_images, action: :linkable_things
+      post 'thing_images', controller: :thing_images, action: :create
+      get 'thing_images', controller: :thing_images, action: :image_things
+      get 'linkable_things', controller: :thing_images, action: :linkable_things
     end
     resources :things, except: %i[new: edit:] do 
       resources :thing_images, only: %i[index create update destroy]
