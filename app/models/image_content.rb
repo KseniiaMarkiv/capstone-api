76fa2d5e5813b44e1879d1c7d5466071773f118d
Kseniia_Markiv <ksenya.markiv@gmail.com>
2022-08-28 01:28:43 +0300

    added image content model, helper and download

---
+++ app/models/image_content.rb
@@ -0,0 +1,10 @@
+class ImageContent
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  field :image_id, type: Integer
+  field :width, type: Integer
+  field :height, type: Integer
+  field :content_type, type: String
+  field :content, type: BSON::Binary
+  field :original, type: Mongoid::Boolean
+end
