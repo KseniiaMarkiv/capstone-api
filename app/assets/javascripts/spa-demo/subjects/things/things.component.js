28e4da4f83012bea5f53a0e0918d39c5a8453845
Kseniia_Markiv <ksenya.markiv@gmail.com>
2022-09-16 00:30:23 +0300

    start 01_UI Layout

--- app/assets/javascripts/spa-demo/subjects/things/things.component.js
+++ app/assets/javascripts/spa-demo/subjects/things/things.component.js
@@ -51,7 +51,7 @@
         vm.updateImageLinks = updateImageLinks;
 
         vm.$onInit = function() {
-            console.log("ThingEditorController", $scope);
+            // console.log("ThingEditorController", $scope);
             $scope.$watch(function() { return Authz.getAuthorizedUserId(); },
                 function() {
                     if ($stateParams.id) {
@@ -72,7 +72,7 @@
 
         function reload(thingId) {
             var itemId = thingId ? thingId : vm.item.id;
-            console.log("re/loading thing", itemId);
+            // console.log("re/loading thing", itemId);
             vm.images = ThingImage.query({ thing_id: itemId });
             vm.item = Thing.get({ id: itemId });
             vm.thingsAuthz.newItem(vm.item);
@@ -99,7 +99,7 @@
             vm.item.errors = null;
             vm.item.$save().then(
                 function() {
-                    console.log("thing created", vm.item);
+                    // console.log("thing created", vm.item);
                     $state.go(".", { id: vm.item.id });
                 },
                 handleError);
@@ -117,7 +117,7 @@
         }
 
         function updateImageLinks(promise) {
-            console.log("updating links to images");
+            // console.log("updating links to images");
             var promises = [];
             if (promise) { promises.push(promise); }
             angular.forEach(vm.images, function(ti) {
@@ -128,10 +128,10 @@
                 }
             });
 
-            console.log("waiting for promises", promises);
+            // console.log("waiting for promises", promises);
             $q.all(promises).then(
                 function(response) {
-                    console.log("promise.all response", response);
+                    // console.log("promise.all response", response);
                     //update button will be disabled when not $dirty
                     $scope.thingform.$setPristine();
                     reload();
@@ -142,7 +142,7 @@
         function remove() {
             vm.item.$remove().then(
                 function() {
-                    console.log("thing.removed", vm.item);
+                    // console.log("thing.removed", vm.item);
                     clear();
                 },
                 handleError);
@@ -171,7 +171,7 @@
         var vm = this;
 
         vm.$onInit = function() {
-            console.log("ThingSelectorController", $scope);
+            // console.log("ThingSelectorController", $scope);
             $scope.$watch(function() { return Authz.getAuthorizedUserId(); },
                 function() {
                     if (!$stateParams.id) {
