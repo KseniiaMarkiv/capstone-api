4473ad819c8906c019658b34364c0dcf5dcefb62
Kseniia_Markiv <ksenya.markiv@gmail.com>
2022-09-26 18:49:55 +0300

    added functional list of current images

---
+++ app/assets/javascripts/spa-demo/subjects/current_images/current_images.component.js
@@ -0,0 +1,37 @@
+(function() {
+    "use strict";
+
+    angular
+        .module("spa-demo.subjects")
+        .component("sdCurrentImages", {
+            templateUrl: imagesTemplateUrl,
+            controller: CurrentImagesController,
+        });
+
+
+    imagesTemplateUrl.$inject = ["spa-demo.config.APP_CONFIG"];
+
+    function imagesTemplateUrl(APP_CONFIG) {
+        return APP_CONFIG.current_images_html;
+    }
+
+    CurrentImagesController.$inject = ["$scope",
+        "spa-demo.subjects.currentSubjects"
+    ];
+
+    function CurrentImagesController($scope, currentSubjects) {
+        var vm = this;
+
+        vm.$onInit = function() {
+            console.log("CurrentImagesController", $scope);
+        }
+        vm.$postLink = function() {
+            $scope.$watch(
+                function() { return currentSubjects.getImages(); },
+                function(images) { vm.images = images; }
+            );
+        }
+        return;
+        //////////////
+    }
+})();
